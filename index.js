0a5acad2b640aaef15acf235123cff43c1bde0b6
Lucas Andrade de Lara <lscynz1995@gmail.com>
2021-09-13 13:40:28 -0300

    Finalizado o bug com imagens duplicadas

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const tesoura = document.getElementById("scissor")
 const abrir = document.getElementById("aprenderJogar")
 const player = document.getElementById("player")
 const reset = document.getElementById("reset")
+const paragrafos = document.getElementById("textoDePlay")
 
 const pedrona = () => 
 {
@@ -31,21 +32,22 @@ const tesourona = () =>
   player.appendChild(playerTesoura)
 }
 
-const papelPlayer = () => 
-{
-  const playerPapel = document.createElement("img")  
-  playerPapel.classList.add("xx")
-  playerPapel.src = "papel.png"
-  player.appendChild(playerPapel)
-}    
-
 const pedraPlayer = () => 
 {
   const playerPedra = document.createElement("img")
   playerPedra.classList.add("xx")
   playerPedra.src = "pedra.png"
   player.appendChild(playerPedra)
+  return "1"
 }
+const papelPlayer = () => 
+{
+  const playerPapel = document.createElement("img")  
+  playerPapel.classList.add("xx")
+  playerPapel.src = "papel.png"
+  player.appendChild(playerPapel)
+  return "2"
+}    
 
 const tesouraPlayer = () => 
 {
@@ -53,6 +55,7 @@ const tesouraPlayer = () =>
   playerTesoura.classList.add("xx")
   playerTesoura.src = "tesoura.png"
   player.appendChild(playerTesoura)
+  return "3"
 }
 
 const rolagem3 = () => 
@@ -60,11 +63,26 @@ const rolagem3 = () =>
   const rolagem = Math.floor(Math.random() * 3) + 1
   if (rolagem === 1) {
     pedrona()
+    return "1"
   } else if ( rolagem === 2 ) {
     papelzao()
+    return "2"
   } else {
     tesourona()
+    return "3"
   }
+}
+
+const criarTexto = () => 
+{
+  const criarText = document.createElement("p")
+  criarText.innerHTML = "Player"
+  criarText.classList.add("bonito")
+  player.appendChild(criarText)
+  const criarText2 = document.createElement("p")
+  criarText2.innerHTML = "Maquina"
+  criarText2.classList.add("bot")
+  player.appendChild(criarText2)
 }
 const apagarTudo = () => 
 {
@@ -84,6 +102,12 @@ const resetar = () => {
   reset.classList.remove("hide")
 }
 
+const sumiu = () => {
+  paragrafos.classList.add("hide")
+  comoJogar.classList.add("hide")
+}
+
+
 // EVENTO DE CLICK PARA SABER COMO JOGAR //
 comoJogar.addEventListener("click", () => 
 { 
@@ -101,39 +125,53 @@ comoJogar.addEventListener("click", () =>
 
 reset.addEventListener("click", () => 
 {
-  // const lista = document.getElementsByClassName("tryInOne")
-  // player.removeChild(lista[1])
-  // player.removeChild(lista[2])
-  player.classList.add("hide")
-  aparecerTudo()
-  reset.classList.add("hide")
+  location.reload()
 })
 
 pedra.addEventListener("click", () => {
   apagarTudo()
-  pedraPlayer()
-  rolagem3()
   resetar()
-  // PEDIR DICA DE COMO FAZER ISSO
-  // if (rolagem3().value === pedraPlayer) {
-    //   const perdeu = document.createElement("p")
-    //   perdeu.innerHTML = "Que pena, voce perdeu, mas nao fique triste, havera outras derrotas. Muahahaa!!!"
-    //   player.appendChild(perdeu)
-    // }
-  })
+  criarTexto()
+  sumiu()
+  rolagem3()
+  pedraPlayer()
+})
 
-  papel.addEventListener("click", () => {
-    apagarTudo()
-    papelPlayer()
-    rolagem3()
-    resetar()
-  })
+papel.addEventListener("click", () => {
+  apagarTudo()
+  resetar()
+  criarTexto()
+  sumiu()
+  papelPlayer()
+  rolagem3()
+  // const minVar = rolagem3()
+  // const outraVar = papelPlayer()
+  // const maisUmaVar = pedraPlayer()
+  // const outraAinda = tesouraPlayer()
+  // if (minVar == outraAinda) {
+  //     const perdeu = document.createElement("p")
+  //     perdeu.classList.add("derrota")
+  //     perdeu.innerHTML = "Que pena, voce perdeu, mas nao fique triste, havera outras derrotas. Muahahaa!!!"
+  //     player.appendChild(perdeu)
+  //   } else if (minVar === outraVar) {
+  //     const empate = document.createElement("p")
+  //     empate.classList.add("empate")
+  //     empate.innerHTML = "Que sorte a sua, empatou, mas na proxima voce nao escapara!!!"
+  //     player.appendChild(empate)
+  //   } else if (minVar == maisUmaVar) {
+  //     const ganhou = document.createElement("p")
+  //     ganhou.classList.add("vitoria")
+  //     ganhou.innerHTML = "Como ousa ganhar da minha pessoa? Voce nao ve que tera consequencias extratosfericas!?"
+  //     player.appendChild(ganhou)
+  //   }
+})
 
-  tesoura.addEventListener("click", () => {
-    apagarTudo()
-    tesouraPlayer()
-    rolagem3()
-    resetar()
-  })
-  
-  
+tesoura.addEventListener("click", () => {
+  apagarTudo()
+  resetar()
+  criarTexto()
+  sumiu()
+  rolagem3()
+  tesouraPlayer()
+})
+
